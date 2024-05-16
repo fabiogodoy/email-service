c6d980cbc4b8889c4198045f1cb64c7e7f14edf4
Fabio Godoy <godoy@MacBook-Pro-de-Fabio.local>
2024-05-16 09:06:23 -0300

    Still fixing gitignore

--- build.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-
-rm -rf target
-mvn package
-mv target/email-service-1.0.war $TOMCAT_HOME/webapps
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-
-rm -rf target
-mvn package
-scp -i ~/Documents/certificates/pilatespro.pem target/email-service-1.0.war ubuntu@server1.connectas.com.br:~/
